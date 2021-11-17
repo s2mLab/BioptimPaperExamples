2ababcc2d8ed071a0f1a5c83a38f4d9a202453b1
Pariterre <pariterre@hotmail.com>
2021-11-17 16:06:19 -0500

    Updated parameters for Jumper

--- jumper/main.py
+++ jumper/main.py
@@ -2,17 +2,19 @@ from time import time
 
 from bioptim import ControlType
 
-from JumperOcp import JumperOcp, Jumper, OdeSolver
+from JumperOcp import JumperOcp, JumperCOLLOCATION
+
 
 if __name__ == "__main__":
     root_path_model = "/".join(__file__.split("/")[:-1])
-    jumper_model = Jumper(root_path_model + "/models/")
-    ode_solver = OdeSolver.RK4()
-    jumper = JumperOcp(jumper=jumper_model, control_type=ControlType.CONSTANT, n_phases=2, ode_solver=ode_solver)
+    jumper_model = JumperCOLLOCATION(root_path_model + "/models/")
+    jumper = JumperOcp(jumper=jumper_model, control_type=ControlType.CONSTANT, n_phases=2)
 
     tic = time()
-    sol = jumper.solve(limit_memory_max_iter=200, exact_max_iter=1000)
-    print(f"Time to solve : {time() - tic}sec")
-
+    sol = jumper.solve(
+        limit_memory_max_iter=jumper_model.n_max_iter_limited_memory,
+        exact_max_iter=jumper_model.n_max_iter_exact,
+    )
     sol.print()
+    print(f"Time to solve : {time() - tic}sec")
     sol.animate()
